1243b71d6ebae244582467d691f5807b80783b77
Tim Rye <tim@tenfold.media>
2016-02-17 17:02:48 +0000

    Add functionality from Tenfold Redirect to Blog Base

--- tenfold-base.php
+++ tenfold-base.php
@@ -13,3 +13,16 @@ GitHub Branch:     master
 
 /* Require and reccommend plugins */
 require_once dirname( __FILE__ ).'/plugins.php';
+
+
+/* Redirect to '/blog/' base */
+function tf_redirect_to_blog_base($template) {
+	global $wp_rewrite, $wp_query;
+	if (!is_404() || $wp_rewrite->permalink_structure !== '/blog/%postname%/') return $template;
+	$url = '/blog'.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	if (!$postID = url_to_postid($url)) return $template;
+	$url = get_permalink($postID).(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) ? '?'.parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) : '').(parse_url($_SERVER['REQUEST_URI'], PHP_URL_FRAGMENT) ? '#'.parse_url($_SERVER['REQUEST_URI'], PHP_URL_FRAGMENT) : '');
+    wp_redirect($url, 301);
+    exit;
+}
+add_filter('404_template', 'tf_redirect_to_blog_base');
