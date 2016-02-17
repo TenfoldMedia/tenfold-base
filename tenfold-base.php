213f846b495b3ea8a0b89804043828138906fa73
Tim Rye <tim@tenfold.media>
2016-02-17 17:00:58 +0000

    Initial plugin setup

---
+++ tenfold-base.php
@@ -0,0 +1,11 @@
+<?php
+/******************************************************************
+Plugin Name:       Tenfold Base
+Plugin URI:        http://tenfold.media
+Description:       This plugin helps us set up WordPress for Tenfold Media clients.
+Author:            Tim Rye
+Author URI:        http://tenfold.media/tim
+Version:           1.0.0
+GitHub Plugin URI: TenfoldMedia/tenfold-base
+GitHub Branch:     master
+******************************************************************/
